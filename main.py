2193e88574cdcb5129aed50904062f77f0418028
lancelot <lancelot.b@outlook.com>
2016-03-31 20:19:13 +0800

    310316#01: nop

--- main.py
+++ main.py
@@ -23,38 +23,17 @@ print (sensor.val(sensors[0][2]))
 print (sensor.val(sensors[0][1]))
 print (sensor.val(sensors[0][2]))
 
-def pid(m1, m2, sen1, speed, black, kp, ki, kd):
-	intg = 0
-	derv = 0
-	last_err = 0
-	motor.runDoubleDirect(m1, m2, speed, speed)
-	for i in range(0, 500):
-		val = sensor.val(sen1)
-		error = val - black
-		intg += error
-		derv = error - last_err
-		output = kp * error + ki * intg + kd * derv
+# m1/2: motor 1/2
+# sen1/2: sensor 1/2
+# speed: ideal speed
+# tc1/2: target color 1/2
 
-		s1 = speed + output
-		s2 = speed - output
+# black1/2: reflection for black(of sensor 1/2), used in cross line process
+# white1/2: reflection for white(of sensor 1/2), used in break line process
 
-		if (s1 > 100):
-			s1 = 100
-		elif (s1 < -100):
-			s1 = -100
+# kp, ki, kd: coefficients for PID
 
-		if (s2 > 100):
-			s2 = 100
-		elif (s2 < -100):
-			s2 = -100
-
-		print(str(int(s1)) + ", " + str(int(s2)))
-		motor.setDoubleSpeed(m1, m2, int(s1), int(s2))
-		last_err = error
-	motor.stop(motors[1])
-	motor.stop(motors[2])
-
-def pid2(m1, m2, sen1, sen2, speed, black1, black2, white1, white2, kp, ki, kd):
+def pid2(m1, m2, sen1, sen2, speed, tc1, tc2, black1, black2, white1, white2, kp, ki, kd):
 	intg1 = 0
 	derv1 = 0
 	last_err1 = 0
@@ -73,19 +52,32 @@ def pid2(m1, m2, sen1, sen2, speed, black1, black2, white1, white2, kp, ki, kd):
 	origin_speed = speed
 	origin_kp = kp
 	origin_ki = ki
+	origin_kd = kd
 
 	turn_count = 0
 	keep_turn = 0
 	is_in_dec = 0
 
+	dec_threshold = 55
+	origin_dec_threshold = dec_threshold
+
 	motor.runDoubleDirect(m1, m2, speed, speed)
-	for i in range(0, 1200):
+	for i in range(0, 2000):
 		val1 = sensor.val(sen1)
 		val2 = sensor.val(sen2)
+
+		"""
+		if (val1 <= black1 and val2 <= black2):
+			motor.runDoubleRelat(m1, m2, 50, 150, 50, 150)
+			motor.waitForStop(m1)
+			motor.runDoubleRelat(m1, m2, 50, 360, 50, -360)
+			motor.waitForStop(m1)
+			motor.runDoubleDirect(m1, m2, speed, speed)
+		"""
 		
 		if (val1 >= white1 and val2 >= white2):
 			white_count += 1
-			if (white_count > 62):
+			if ((not is_in_dec and white_count > 70) or (is_in_dec and white_count > 100)):
 				print("!!! too much white")
 				pos1_end = motor.getPos(m1)
 				pos2_end = motor.getPos(m2)
@@ -107,34 +99,47 @@ def pid2(m1, m2, sen1, sen2, speed, black1, black2, white1, white2, kp, ki, kd):
 					if (val1 <= white1 or val2 <= white2):
 						break
 				"""
-
 				while 1:
-					motor.runDoubleRelat(m1, m2, 60, 100, 60, 100)
-					motor.waitForStop(m1)
-					pos1_begin = motor.getPos(m1)
-					pos2_begin = motor.getPos(m2)
-					motor.runDoubleRelat(m1, m2, 60, 500, 60, -500)
-					motor.waitForStop(m1)
-					count = 0
-					while count < 12:
-						val1 = sensor.val(sen1)
-						# print(str(val2))
+					try_times = 0
+					while try_times < 3:
+						try_times += 1
+						motor.runDoubleRelat(m1, m2, 60, 100, 60, 100)
+						motor.waitForStop(m1)
+						pos1_begin = motor.getPos(m1)
+						pos2_begin = motor.getPos(m2)
+						motor.runDoubleRelat(m1, m2, 60, 500, 60, -500)
+						motor.waitForStop(m1)
+						count = 0
+						while count < 20:
+							val1 = sensor.val(sen1)
+							# print(str(val2))
+							if (val1 <= white1):
+								print("restore")
+								break
+							motor.runDoubleRelat(m1, m2, 60, -40, 60, 40)
+							motor.waitForStop(m2)
+							count += 1
 						if (val1 <= white1):
-							print("restore")
 							break
-						motor.runDoubleRelat(m1, m2, 60, -80, 60, 80)
-						motor.waitForStop(m2)
-						count += 1
-					if (val1 <= white1):
-						break
+						else:
+							pos1_end = motor.getPos(m1)
+							pos2_end = motor.getPos(m2)
+							motor.runDoubleRelat(m1, m2,
+												 50, pos1_begin - pos1_end + 50,
+												 50, pos2_begin - pos2_end + 50,
+												 "hold")
+							motor.waitForDoubleHold(m1, m2)
+
+					if (try_times >= 3):
+						print("HOLY SH--!!")
+						motor.runDoubleRelat(m1, m2, 60, (-try_times - 2) * 100, 60, (-try_times - 2) * 100)
+						motor.waitForStop(m1)
+						print("let's change a direction")
+						motor.runDoubleRelat(m1, m2, 60, -100, 60, 100)
+						motor.waitForStop(m1)
 					else:
-						pos1_end = motor.getPos(m1)
-						pos2_end = motor.getPos(m2)
-						motor.runDoubleRelat(m1, m2,
-											 50, pos1_begin - pos1_end + 50,
-											 50, pos2_begin - pos2_end + 50,
-											 "hold")
-						motor.waitForDoubleHold(m1, m2)
+						break
+				# end here
 
 				motor.runDoubleDirect(m1, m2, speed, speed)
 				white_count = 0
@@ -147,13 +152,13 @@ def pid2(m1, m2, sen1, sen2, speed, black1, black2, white1, white2, kp, ki, kd):
 		# print("val1: %s; val2: %s; white count: %s" % (val1, val2, white_count))
 		
 
-		error1 = val1 - black1
+		error1 = val1 - tc1
 		intg1 += error1
 		intg1 *= 1 - ki
 		derv1 = error1 - last_err1
 		output1 = kp * error1 + ki * intg1 + kd * derv1
 
-		error2 = val2 - black2
+		error2 = val2 - tc2
 		intg2 += error2
 		intg2 *= 1 - ki
 		derv2 = error2 - last_err2
@@ -174,35 +179,46 @@ def pid2(m1, m2, sen1, sen2, speed, black1, black2, white1, white2, kp, ki, kd):
 		elif (s2 < -100):
 			s2 = -100
 
-		if (abs(s1 - s2) > 120):
+		# print(abs(s1 - s2))
+		
+		if (abs(s1 - s2) > dec_threshold):
 			turn_count += 1
-			print("turn count: " + str(turn_count))
-			if (turn_count >= 3):
-				if (is_in_dec):
-					keep_turn = 50 # keep this config for at least 10 turns
-					print("deceleration start")
-				else:
+			# print("turn count: " + str(turn_count))
+			if (turn_count >= 4 or (is_in_dec and turn_count >= 3)):
+				print("deceleration start")
+				if (not is_in_dec):
 					is_in_dec = 1
-					speed = 30
-					kp = 4.4
+					speed = 20
+					kp = 4.2
 					ki = 0.001
-					# val2 -= 5
+					kd = 40
+					dec_threshold = 185
+					keep_turn = 80
+				else:
+					# kp = 3.8
+					keep_turn += 20
+				
+				# val2 -= 5
 				turn_count = 0
 				
 		else:
 			turn_count = 0
 			if (is_in_dec):
-				if (keep_turn <= 0):
+				if (keep_turn == 1):
 					keep_turn = 0
 					print("restore speed")
 					is_in_dec = 0
 					speed = origin_speed
 					kp = origin_kp
 					ki = origin_ki
+					kd = origin_kd
+					intg1 = 0
+					intg2 = 0
+
+					dec_threshold = origin_dec_threshold
 					# val2 += 5
 				else:
 					keep_turn -= 1
-			
 
 		# print(str(int(s1)) + ", " + str(int(s2)))
 		motor.setDoubleSpeed(m1, m2, int(s1), int(s2))
@@ -223,10 +239,10 @@ motor.setPolarity(motors[2], "inversed")
 # pid(motors[1], motors[2], sensors[0][1], 30, 13, 5, 0.001, 40)
 
 val1 = sensor.val(sensors[0][1])
-val2 = sensor.val(sensors[0][2]) - 8
+val2 = sensor.val(sensors[0][2]) - 10
 
 print("val1: %s, val2: %s" % (val1, val2))
 
-pid2(motors[1], motors[2], sensors[0][1], sensors[0][2], 40, val1, val2, 20, 30, 3, 0.01, 40)
+pid2(motors[1], motors[2], sensors[0][1], sensors[0][2], 40, val1, val2, 20, 20, 22, 32, 1.5, 0.04, 70)
 
 # motor.runDoubleRelat(motors[1], motors[2], 60, 50000, 60, 50000)
